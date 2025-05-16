f6f4895ac32133b1435c2fdad84c5f9279554d21
Herbert <herbert.woisetschlaeger@tum.de>
2025-05-15 22:06:58 -0400

    Updated sampling condition

--- utils/mess_plus/bernoulli_sampler.py
+++ utils/mess_plus/bernoulli_sampler.py
@@ -5,7 +5,7 @@ from numpy.random import binomial
 
 def sample_from_bernoulli(c: float, timestamp: int):
     p_t = min(
-        1.0, c / np.power(1 if timestamp == 0 else timestamp, (1 / 5))
+        1.0, c / np.power(1 if timestamp == 0 else timestamp, (1 / 4))
     )
 
     x_t = binomial(n=1, p=p_t, size=1)
